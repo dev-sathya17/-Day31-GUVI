3f9d8142242e72dfe34f7db9447908a1a9de0f92
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-07 16:37:56 +0530

    added navbar and styling of home page started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import Navbar from "../components/navbar/Navbar";
+import { FaCircleArrowRight } from "react-icons/fa6";
+import "./Home.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="section-container">
+        <section>
+          <h1>Our Authors</h1>
+          <FaCircleArrowRight />
+        </section>
+        <section>
+          <h1>Our Books</h1>
+          <FaCircleArrowRight />
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
