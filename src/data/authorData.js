da7dee24dba9251af1ba2050e9c6f27e65de88f1
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-07 14:48:09 +0530

    added local data for books and authors

---
+++ src/data/authorData.js
@@ -0,0 +1,42 @@
+export const authors = [
+  {
+    id: 1,
+    name: "J.K. Rowling",
+    dob: "31-07-1965",
+    bio: "J.K. Rowling is a British author best known for writing the Harry Potter fantasy series, which has become one of the best-selling book series in history. Born in Yate, Gloucestershire, she conceived the idea for the series while on a delayed train from Manchester to London in 1990. The success of the books and films has made Rowling one of the world's most famous and wealthiest authors. Beyond Harry Potter, she has written adult fiction and crime novels under the pseudonym Robert Galbraith.",
+    image:
+      "https://m.media-amazon.com/images/S/amzn-author-media-prod/8cigckin175jtpsk3gs361r4ss.jpg",
+  },
+  {
+    id: 2,
+    name: "George Orwell",
+    dob: "25-06-1903",
+    bio: "George Orwell, born Eric Arthur Blair, was an English novelist, essayist, journalist, and critic. His work is marked by lucid prose, biting social criticism, and outspoken support for democratic socialism. Orwell is best known for his dystopian novel '1984' and the allegorical novella 'Animal Farm.' Both works critique totalitarianism and have had a profound impact on popular and political culture.",
+    image:
+      "https://www.neh.gov/sites/default/files/styles/large/public/2019-01/2019_01-Winter_Orwell_01_0.jpg?itok=sN7QYAHi",
+  },
+  {
+    id: 3,
+    name: "Jane Austen",
+    dob: "16-12-1775",
+    bio: "Jane Austen was an English novelist known primarily for her six major novels, including 'Pride and Prejudice,' 'Sense and Sensibility,' and 'Emma.' Her novels critique the British landed gentry at the end of the 18th century and are notable for their wit, social observation, and insights into the lives of women. Austen's works have inspired countless adaptations and remain beloved around the world.",
+    image:
+      "https://media.newyorker.com/photos/5c143d27d2db9849ec9e3baf/1:1/w_1820,h_1820,c_limit/960108_ra604.jpg",
+  },
+  {
+    id: 4,
+    name: "Gabriel García Márquez",
+    dob: "06-03-1927",
+    bio: "Gabriel García Márquez was a Colombian novelist, short-story writer, screenwriter, and journalist. Considered one of the most significant authors of the 20th century, he was awarded the Nobel Prize in Literature in 1982. His most famous works include 'One Hundred Years of Solitude' and 'Love in the Time of Cholera,' which are hallmarks of magical realism, blending the fantastic with the ordinary.",
+    image:
+      "https://www.agenciabalcells.com/fileadmin/_processed_/csm_garcia_marquez__gabriel_f26894719d.jpg",
+  },
+  {
+    id: 5,
+    name: "Agatha Christie",
+    dob: "15-09-1890",
+    bio: "Agatha Christie was an English writer known for her 66 detective novels and 14 short story collections, particularly those revolving around her famous detectives Hercule Poirot and Miss Marple. She also wrote the world's longest-running play, 'The Mousetrap.' Christie is often referred to as the 'Queen of Mystery' and is one of the best-selling authors of all time.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/cf/Agatha_Christie.png",
+  },
+];
---
+++ src/data/bookData.js
@@ -0,0 +1,37 @@
+export const books = [
+  {
+    id: 1,
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J.K. Rowling",
+    ISBN: "978-0747532699",
+    publicationDate: "26-06-1997",
+  },
+  {
+    id: 2,
+    title: "1984",
+    author: "George Orwell",
+    ISBN: "978-0451524935",
+    publicationDate: "08-06-1949",
+  },
+  {
+    id: 3,
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    ISBN: "978-1503290563",
+    publicationDate: "28-01-1813",
+  },
+  {
+    id: 4,
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel García Márquez",
+    ISBN: "978-0060883287",
+    publicationDate: "05-06-1967",
+  },
+  {
+    id: 5,
+    title: "Murder on the Orient Express",
+    author: "Agatha Christie",
+    ISBN: "978-0062693662",
+    publicationDate: "01-01-1934",
+  },
+];
